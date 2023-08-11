7b1f4c053a0dd825722b9b91d26561ca9d24ba01
Milthon <menteprospera3000@gmail.com>
2023-08-11 15:12:13 -0500

    NewCommiF

--- src/Controllers/Administrador/procesar_editar.php
+++ src/Controllers/Administrador/procesar_editar.php
@@ -2,11 +2,11 @@
 require_once($_SERVER["DOCUMENT_ROOT"] . "/src/Model/connection.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id_ud = $_POST["id_ud"];  // Obtener el valor del parámetro id_ud
+    $id_ud = $_POST["id_ud"];  
 
-    $rol = $_POST["rol"];  // Obtener el valor del campo de selección de rol
-    $email = $_POST["Email"];  // Obtener el valor del campo de email
-    // Aquí debes realizar las validaciones y saneamiento de datos necesarios
+    $rol = $_POST["rol"];  
+    $email = $_POST["Email"];  
+  
 
     $sql = "UPDATE usuarios_login SET correo = :correo, id_rol = :id_rol WHERE id_ul = :id_ud";
     $stmt = $pdo->prepare($sql);
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->bindParam(":id_ud", $id_ud);
     $stmt->execute();
 
-    // Puedes redirigir al usuario a otra página después de la actualización
-    header("Location: /src/Views/Administrador/Formularios/EditarForm.php?id_ud=" . $id_ud . "&success=1");
+  
+    header("Location: /src/Views/Administrador/Formularios/EditarForm.php?id_ud=" . $id_ud);
     exit();
 }
